319c174f173fb80ea80437eb1c4f32272337302a
ojh31 <oskar.hollinsworth@gmail.com>
2022-11-29 09:14:26 +0000

    First attempt at train_dqn with while loop

--- w4d2_chapter4_dqn/dqn.py
+++ w4d2_chapter4_dqn/dqn.py
@@ -23,6 +23,7 @@ from numpy.random import Generator
 import gym.envs.registration
 import pandas as pd
 import random
+import wandb
 from w3d5_chapter4_tabular.utils import make_env
 from w4d2_chapter4_dqn import utils
 
@@ -225,9 +226,359 @@ def epsilon_greedy_policy(
         pi = rng.integers(0, envs.single_action_space.n, envs.num_envs)
     else:
         action_values = q_network(obs)
-        pi = action_values.argmax(dim=1).detach().numpy()
+        pi = action_values.detach().argmax(dim=1).numpy()
     return pi
 
 if MAIN:
     utils.test_epsilon_greedy_policy(epsilon_greedy_policy)
 # %%
+ObsType = np.ndarray
+ActType = int
+
+class Probe1(gym.Env):
+    '''One action, observation of [0.0], one timestep long, +1 reward.
+
+    We expect the agent to rapidly learn that the value of the constant [0.0] observation 
+    is +1.0. Note we're using a continuous observation space for consistency with CartPole.
+    '''
+
+    action_space: Discrete
+    observation_space: Box
+
+    def __init__(self):
+        super().__init__()
+        self.observation_space = Box(np.array([0]), np.array([0]))
+        self.action_space = Discrete(1)
+        self.seed()
+        self.reset()
+
+    def step(self, action: ActType) -> tuple[ObsType, float, bool, dict]:
+        return (np.array([0]), 1.0, True, {})
+
+    def reset(
+        self, seed: Optional[int] = None, return_info=False, options=None
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        if return_info:
+            return (np.array([0.0]), {})
+        return np.array([0.0])
+
+gym.envs.registration.register(id="Probe1-v0", entry_point=Probe1)
+if MAIN:
+    env = gym.make("Probe1-v0")
+    assert env.observation_space.shape == (1,)
+    assert env.action_space.shape == ()
+# %%
+class Probe2(gym.Env):
+    '''One action, observation of [-1.0] or [+1.0], one timestep long, reward equals observation.
+
+    We expect the agent to rapidly learn the value of each observation is equal to the observation.
+    '''
+
+    action_space: Discrete
+    observation_space: Box
+
+    def __init__(self):
+        pass
+
+    def step(self, action: ActType) -> tuple[ObsType, float, bool, dict]:
+        pass
+
+    def reset(
+        self, seed: Optional[int] = None, return_info=False, options=None
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        pass
+
+gym.envs.registration.register(id="Probe2-v0", entry_point=Probe2)
+
+class Probe3(gym.Env):
+    '''One action, [0.0] then [1.0] observation, two timesteps, +1 reward at the end.
+
+    We expect the agent to rapidly learn the discounted value of the initial observation.
+    '''
+
+    action_space: Discrete
+    observation_space: Box
+
+    def __init__(self):
+        pass
+
+    def step(self, action: ActType) -> tuple[ObsType, float, bool, dict]:
+        pass
+
+    def reset(
+        self, seed: Optional[int] = None, return_info=False, options=None
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        pass
+
+gym.envs.registration.register(id="Probe3-v0", entry_point=Probe3)
+
+class Probe4(gym.Env):
+    '''Two actions, [0.0] observation, one timestep, reward is -1.0 or +1.0 dependent on the action.
+
+    We expect the agent to learn to choose the +1.0 action.
+    '''
+
+    action_space: Discrete
+    observation_space: Box
+
+    def __init__(self):
+        pass
+
+    def step(self, action: ActType) -> tuple[ObsType, float, bool, dict]:
+        pass
+
+    def reset(
+        self, seed: Optional[int] = None, return_info=False, options=None
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        pass
+
+gym.envs.registration.register(id="Probe4-v0", entry_point=Probe4)
+
+class Probe5(gym.Env):
+    '''Two actions, random 0/1 observation, one timestep, reward is 1 if action equals observation otherwise -1.
+
+    We expect the agent to learn to match its action to the observation.
+    '''
+
+    action_space: Discrete
+    observation_space: Box
+
+    def __init__(self):
+        pass
+
+    def step(self, action: ActType) -> tuple[ObsType, float, bool, dict]:
+        pass
+
+    def reset(
+        self, seed: Optional[int] = None, return_info=False, options=None
+    ) -> Union[ObsType, tuple[ObsType, dict]]:
+        pass
+
+gym.envs.registration.register(id="Probe5-v0", entry_point=Probe5)
+# %%
+@dataclass
+class DQNArgs:
+    exp_name: str = os.path.basename(
+        globals().get("__file__", "DQN_implementation").rstrip(".py")
+    )
+    seed: int = 1
+    torch_deterministic: bool = True
+    cuda: bool = True
+    track: bool = False
+    wandb_project_name: str = "CartPoleDQN"
+    wandb_entity: Optional[str] = None
+    capture_video: bool = False
+    env_id: str = "CartPole-v1"
+    total_timesteps: int = 500000
+    learning_rate: float = 0.00025
+    buffer_size: int = 10000
+    gamma: float = 0.99
+    target_network_frequency: int = 500
+    batch_size: int = 128
+    start_e: float = 1.0
+    end_e: float = 0.05
+    exploration_fraction: float = 0.5
+    learning_starts: int = 10000
+    train_frequency: int = 10
+
+arg_help_strings = dict(
+    exp_name = "the name of this experiment",
+    seed = "seed of the experiment",
+    torch_deterministic = "if toggled, `torch.backends.cudnn.deterministic=False`",
+    cuda = "if toggled, cuda will be enabled by default",
+    track = "if toggled, this experiment will be tracked with Weights and Biases",
+    wandb_project_name = "the wandb's project name",
+    wandb_entity = "the entity (team) of wandb's project",
+    capture_video = "whether to capture videos of the agent performances (check out `videos` folder)",
+    env_id = "the id of the environment",
+    total_timesteps = "total timesteps of the experiments",
+    learning_rate = "the learning rate of the optimizer",
+    buffer_size = "the replay memory buffer size",
+    gamma = "the discount factor gamma",
+    target_network_frequency = "the timesteps it takes to update the target network",
+    batch_size = "the batch size of samples from the replay memory",
+    start_e = "the starting epsilon for exploration",
+    end_e = "the ending epsilon for exploration",
+    exploration_fraction = "the fraction of `total-timesteps` it takes from start-e to go end-e",
+    learning_starts = "timestep to start learning",
+    train_frequency = "the frequency of training",
+)
+toggles = ["torch_deterministic", "cuda", "track", "capture_video"]
+
+def parse_args(arg_help_strings=arg_help_strings, toggles=toggles) -> DQNArgs:
+    parser = argparse.ArgumentParser()
+    for (name, field) in DQNArgs.__dataclass_fields__.items():
+        flag = "--" + name.replace("_", "-")
+        type_function = field.type if field.type != bool else lambda x: bool(strtobool(x))
+        toggle_kwargs = {"nargs": "?", "const": True} if name in toggles else {}
+        parser.add_argument(
+            flag, type=type_function, default=field.default, 
+            help=arg_help_strings[name], **toggle_kwargs
+        )
+    return DQNArgs(**vars(parser.parse_args()))
+
+def setup(args: DQNArgs) -> Tuple[
+    str, SummaryWriter, np.random.Generator, t.device, gym.vector.SyncVectorEnv
+]:
+    '''Helper function to set up useful variables for the DQN implementation'''
+    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+    if args.track:
+        wandb.init(
+            project=args.wandb_project_name,
+            entity=args.wandb_entity,
+            sync_tensorboard=True,
+            config=vars(args),
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+        )
+    writer = SummaryWriter(f"runs/{run_name}")
+    writer.add_text(
+        "hyperparameters",
+        "|param|value|\n|-|-|\n%s" % "\n".join([
+            f"|{key}|{value}|" for (key, value) in vars(args).items()
+        ]),
+    )
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = args.torch_deterministic
+    rng = np.random.default_rng(args.seed)
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
+    envs = gym.vector.SyncVectorEnv([
+        utils.make_env(args.env_id, args.seed, 0, args.capture_video, run_name)
+    ])
+    assert isinstance(envs.single_action_space, Discrete), "only discrete action space is supported"
+    return (run_name, writer, rng, device, envs)
+
+def log(
+    writer: SummaryWriter,
+    start_time: float,
+    step: int,
+    predicted_q_vals: t.Tensor,
+    loss: Union[float, t.Tensor],
+    infos: Iterable[dict],
+    epsilon: float,
+):
+    '''Helper function to write relevant info to TensorBoard logs, and print some things to stdout'''
+    if step % 100 == 0:
+        writer.add_scalar("losses/td_loss", loss, step)
+        writer.add_scalar("losses/q_values", predicted_q_vals.mean().item(), step)
+        writer.add_scalar("charts/SPS", int(step / (time.time() - start_time)), step)
+        if step % 10000 == 0:
+            print("SPS:", int(step / (time.time() - start_time)))
+    for info in infos:
+        if "episode" in info.keys():
+            print(f"global_step={step}, episodic_return={info['episode']['r']}")
+            writer.add_scalar("charts/episodic_return", info["episode"]["r"], step)
+            writer.add_scalar("charts/episodic_length", info["episode"]["l"], step)
+            writer.add_scalar("charts/epsilon", epsilon, step)
+            break
+# %%
+@dataclass
+class Experience:
+    '''A class for storing one piece of experience during an episode run'''
+    obs: ObsType
+    act: ActType
+    reward: float
+    new_obs: ObsType
+    done: bool
+
+
+def train_dqn(args: DQNArgs):
+    (run_name, writer, rng, device, envs) = setup(args)
+    num_actions = envs.single_action_space.n
+    dim_observation = envs.observation_space.shape
+    num_envs = envs.num_envs
+    seed = args.seed
+    q_network = QNetwork(
+        num_actions=num_actions, dim_observation=dim_observation
+    ).train()
+    optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate)
+    buffer = ReplayBuffer(args.buffer_size, num_actions, dim_observation, num_envs, seed)
+    start_time = time.time()
+    obs = envs.reset()
+    target_net = QNetwork(
+        num_actions=num_actions, dim_observation=dim_observation
+    ).eval()
+    for step in range(args.total_timesteps):
+        '''
+        YOUR CODE: Sample actions according to the epsilon greedy policy using the linear 
+        schedule for epsilon, and then step the environment
+        Boilerplate to handle the terminal observation case
+        '''
+        target_net.load_state_dict(q_network.state_dict())
+        epsilon = linear_schedule(
+            step, args.start_e, args.end_e, args.exploration_fraction, args.total_timesteps
+        )
+
+        obs = envs.reset(seed=seed)
+        done = False
+        state_index = 0
+        while not done:
+            act = epsilon_greedy_policy(envs, q_network, rng, obs, epsilon)
+            (new_obs, reward, done, info) = envs.step(act)
+            exp = Experience(obs, act, reward, new_obs, done)
+            buffer.add(obs, actions, rewards, dones, next_obs)
+
+            if step > args.learning_starts and state_index % args.train_frequency == 0:
+                # Learning on this step
+                batch = buffer.sample(args.batch_size)
+                with t.inference_mode():
+                    max_q = target_net(batch.new_observations).max(dim=1)
+                rel_q = q_network(batch.observations)[batch.actions]
+                y = rewards + args.gamma * max_q * done
+                loss = ((y - rel_q) ** 2).sum() / len(y)
+                loss.backward()
+                optimizer.step()
+            if state_index % args.target_network_frequency == 0:
+                # Update target on this step
+                target_net.load_state_dict(q_network.state_dict())
+
+            obs = new_obs
+            state_index += 1
+
+        envs.step()
+        real_next_obs = next_obs.copy()
+        for (i, done) in enumerate(dones):
+            if done:
+                real_next_obs[i] = infos[i]["terminal_observation"]
+        rb.add(obs, actions, rewards, dones, next_obs)
+        obs = next_obs
+        if step > args.learning_starts and step % args.train_frequency == 0:
+            '''
+            "YOUR CODE: Sample from the replay buffer, compute the TD target, compute TD loss, and 
+            perform an optimizer step."
+            '''
+            log(writer, start_time, step, predicted_q_vals, loss, infos, epsilon)
+
+    '''
+    "If running one of the Probe environments, will test if the learned q-values are
+    sensible after training. Useful for debugging.
+    '''
+    probe_batches = [
+        t.tensor([[0.0]]), t.tensor([[-1.0], [+1.0]]), t.tensor([[0.0], [1.0]]), 
+        t.tensor([[0.0]]), t.tensor([[0.0], [1.0]])
+    ]
+    if re.match(r"Probe(\d)-v0", args.env_id):
+        probe_no = int(re.match(r"Probe(\d)-v0", args.env_id).group(1))
+        batch = probe_batches[probe_no]
+        value = q_network(batch)
+        print("Value: ", value)
+        expected = t.tensor([[1.0]]).to(device)
+        t.testing.assert_close(value, expected, 0.0001)
+
+    envs.close()
+    writer.close()
+
+if MAIN:
+    if "ipykernel_launcher" in os.path.basename(sys.argv[0]):
+        filename = globals().get("__file__", "<filename of this script>")
+        print(
+            "Try running this file from the command line instead: "
+            f"python {os.path.basename(filename)} --help"
+        )
+        args = DQNArgs()
+    else:
+        args = parse_args()
+    train_dqn(args)
