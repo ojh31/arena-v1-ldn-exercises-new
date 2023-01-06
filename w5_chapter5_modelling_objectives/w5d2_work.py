a5d6896974cf5c7c84796126103ddd8e816f682f
ojh31 <oskar.hollinsworth@gmail.com>
2023-01-06 17:39:32 +1100

    First run of autoencoder training without errors

--- w5_chapter5_modelling_objectives/w5d2_work.py
+++ w5_chapter5_modelling_objectives/w5d2_work.py
@@ -4,13 +4,11 @@ import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from einops.layers.torch import Rearrange
+from einops import rearrange
 from tqdm.auto import tqdm
+import wandb
+import time
 #%%
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Resize((224, 224)),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
 trainset = datasets.MNIST(
     root="./data", train=True, transform=transforms.ToTensor(), download=True
 )
@@ -18,16 +16,16 @@ testset = datasets.MNIST(
     root="./data", train=False, transform=transforms.ToTensor(), download=True
 )
 
-
 #%%
 class Autoencoder(nn.Module):
     encoder: nn.Sequential
     decoder: nn.Sequential
 
     def __init__(self, img_channels, img_size, out_features, latent_dim_size):
+        super().__init__()
         flatten_size = img_channels * (img_size ** 2)
         self.encoder = nn.Sequential(
-            Rearrange('b c h w -> (b c h w)'),
+            Rearrange('b c h w -> b (c h w)'),
             nn.Linear(flatten_size, out_features),
             nn.ReLU(),
             nn.Linear(out_features, latent_dim_size)
@@ -36,7 +34,7 @@ class Autoencoder(nn.Module):
             nn.Linear(latent_dim_size, out_features),
             nn.ReLU(),
             nn.Linear(out_features, flatten_size),
-            Rearrange('(b c h w) -> b c h w', c=img_channels, h=img_size, w=img_size)
+            Rearrange('b (c h w) -> b c h w', c=img_channels, h=img_size, w=img_size)
         )
 
     def forward(self, x: t.Tensor) -> t.Tensor:
@@ -44,18 +42,26 @@ class Autoencoder(nn.Module):
         x_reconstructed = self.decoder(x_compressed)
         return x_reconstructed
 # %%
-def train_autoencoder():
-    track = False
-    cuda = False
-    epochs = 1
-    device = 'cpu'
-    batch_size = 64
-    autoencoder = Autoencoder(1, 28, 100, 5).train().to(device=device)
+def train_autoencoder(config):
+    device = 'cuda' if config['cuda'] and t.cuda.is_available() else 'cpu'
+    autoencoder = Autoencoder(
+        img_channels=config['img_channels'],
+        img_size=config['img_size'],
+        out_features=config['out_features'],
+        latent_dim_size=config['latent_dim_size'],
+    ).train().to(device=device)
+    batch_size = config['batch_size']
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=True)
-    opt = t.optim.Adam(autoencoder.params())
+    opt = t.optim.Adam(autoencoder.parameters())
+    example_images = next(iter(testloader))
+    last_image_log = time.time()
 
-    for epoch in range(epochs):
+    if config['track']:
+        wandb.init(project='w5d2_autoencoder', config=config)
+
+    n_examples_seen = 0
+    for epoch in range(config['epochs']):
         print(f'Epoch: {epoch + 1}')
 
         for x, y in tqdm(trainloader):
@@ -67,4 +73,38 @@ def train_autoencoder():
             loss.backward()
             opt.step()
 
+            long_since_image_log = (
+                time.time() - last_image_log > config['seconds_between_image_logs']
+            )
+            if config['track'] and long_since_image_log:
+                autoencoder.eval()
+                with t.inference_mode():
+                    new_image_arr = autoencoder(example_images)
+                autoencoder.train()
+                arrays = rearrange(
+                    new_image_arr, "b c h w -> b h w c"
+                ).detach().cpu().numpy()
+                images = [wandb.Image(arr) for arr in arrays]
+                wandb.log(dict(train_loss=loss, images=images), step=n_examples_seen)
+                last_image_log = time.time()
+
+            n_examples_seen += x.shape[0]
+
     return autoencoder
+
+#%%
+autoencoder_config = dict(
+    track = False,
+    cuda = False,
+    epochs = 1,
+    device = 'cpu',
+    batch_size = 64,
+    img_channels=1,
+    img_size=28,
+    out_features=100,
+    latent_dim_size=5,
+    seconds_between_image_logs=10,
+)
+#%%
+trained_autoencoder = train_autoencoder(autoencoder_config)
+#%%
