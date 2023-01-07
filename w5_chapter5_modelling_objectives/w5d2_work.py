7338361a50e45d587a99aceb21b5e047f2c1da5a
ojh31 <oskar.hollinsworth@gmail.com>
2023-01-07 13:05:51 +1100

    Ran training loop of VAE, plotted separate loss functions

--- w5_chapter5_modelling_objectives/w5d2_work.py
+++ w5_chapter5_modelling_objectives/w5d2_work.py
@@ -9,6 +9,7 @@ from tqdm.auto import tqdm
 import wandb
 import numpy as np
 import plotly.express as px
+from typing import Tuple
 #%%
 trainset = datasets.MNIST(
     root="./data", train=True, transform=transforms.ToTensor(), download=True
@@ -175,4 +176,140 @@ fig = px.scatter(
 fig.update_layout(title_x=0.5)
 fig.show()
 
+# %% [markdown]
+#### Variational Autoencoder
+#%%
+# %%
+class VariationalEncoder(nn.Module):
+
+    def __init__(self, img_channels, img_size, out_features, latent_dim_size):
+        super().__init__()
+        flatten_size = img_channels * (img_size ** 2)
+        self.linear_encoder = nn.Sequential(
+            Rearrange('b c h w -> b (c h w)'),
+            nn.Linear(flatten_size, out_features),
+            nn.ReLU(),
+        )
+        self.mean_layer = nn.Linear(out_features, latent_dim_size)
+        self.std_layer = nn.Sequential(
+            nn.Linear(out_features, latent_dim_size),
+        )
+
+    def forward(self, x: t.Tensor) -> Tuple[t.Tensor, t.Tensor, t.Tensor]:
+        '''
+        Returns a tuple of (mu, logsigma, z), where:
+            mu and logsigma are the outputs of your encoder module
+            z is the sampled latent vector taken from distribution N(mu, sigma**2)
+        '''
+        x_common = self.linear_encoder(x)
+        mu = self.mean_layer(x_common)
+        logsigma = self.std_layer(x_common)
+        eps = t.randn_like(mu)
+        z = mu + t.exp(logsigma) * eps
+        return mu, logsigma, z
+# %%
+class VAE(nn.Module):
+    encoder: VariationalEncoder
+    decoder: nn.Sequential
+
+    def __init__(self, img_channels, img_size, out_features, latent_dim_size):
+        super().__init__()
+        flatten_size = img_channels * (img_size ** 2)
+        self.encoder = VariationalEncoder(
+            img_channels, img_size, out_features, latent_dim_size
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim_size, out_features),
+            nn.ReLU(),
+            nn.Linear(out_features, flatten_size),
+            Rearrange('b (c h w) -> b c h w', c=img_channels, h=img_size, w=img_size)
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        x_compressed = self.encoder(x)
+        x_reconstructed = self.decoder(x_compressed)
+        return x_reconstructed
+#%%
+def train_vae(config):
+    t.manual_seed(config['seed'])
+    device = 'cuda' if config['cuda'] and t.cuda.is_available() else 'cpu'
+    vae = VAE(
+        img_channels=config['img_channels'],
+        img_size=config['img_size'],
+        out_features=config['out_features'],
+        latent_dim_size=config['latent_dim_size'],
+    ).train().to(device=device)
+    batch_size = config['batch_size']
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    testloader = DataLoader(testset, batch_size=batch_size, shuffle=True)
+    example_images, example_labels = next(iter(testloader))
+    opt = t.optim.Adam(vae.parameters())
+
+    if config['track']:
+        wandb.init(project=config['project'], config=config)
+
+    n_examples_seen = 0
+    for epoch in range(config['epochs']):
+        print(f'Epoch: {epoch + 1}')
+        epoch_recon_loss = 0
+        epoch_kl_loss = 0
+        epoch_loss = 0
+        epoch_batches = 0
+        for x, y in tqdm(trainloader):
+            x = x.to(device=device)
+            y = y.to(device=device)
+            opt.zero_grad()
+            mu, logsigma, z = vae.encoder(x)
+            sigma = t.exp(logsigma)
+            reconstructed = vae.decoder(z)
+            recon_loss = ((reconstructed - x) ** 2).sum()
+            kl_loss = (0.5 * (mu ** 2 + sigma ** 2 - 1) - logsigma).sum()
+            loss = recon_loss + config['kl_coef'] * kl_loss
+            loss.backward()
+            opt.step()
+            n_examples_seen += x.shape[0]
+            epoch_loss += loss
+            epoch_recon_loss += recon_loss
+            epoch_kl_loss += kl_loss
+            epoch_batches += 1
+
+        if config['track']:
+            vae.eval()
+            with t.inference_mode():
+                _, _, example_z = vae.encoder(example_images)
+                new_image_arr = vae.decoder(example_z)
+            vae.train()
+            arrays = rearrange(
+                new_image_arr, "b c h w -> b h w c"
+            ).detach().cpu().numpy()
+            images = [
+                wandb.Image(arr, caption=example_labels[i]) for i, arr in enumerate(arrays)
+            ]
+            wandb.log(
+                dict(
+                    total_loss=epoch_loss / epoch_batches, 
+                    recon_loss=epoch_recon_loss / epoch_batches, 
+                    kl_loss=epoch_kl_loss / epoch_batches, 
+                    images=images, 
+                ), 
+                step=n_examples_seen
+            )
+
+    return vae.eval().to(device='cpu')
+#%%
+vae_config = dict(
+    project='w5d2_vae',
+    track = True,
+    cuda = False,
+    epochs = 10,
+    batch_size = 64,
+    img_channels=1,
+    img_size=28,
+    out_features=100,
+    latent_dim_size=5,
+    kl_coef=0.1,
+    seed=0,
+)
+# %%
+trained_vae = train_vae(vae_config)
 # %%
