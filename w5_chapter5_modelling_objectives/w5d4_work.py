a81646017dfeefc9a73e25339daa32b9988f05e3
ojh31 <oskar.hollinsworth@gmail.com>
2023-01-13 18:51:01 +1100

    Passed test_vision_attention

--- w5_chapter5_modelling_objectives/w5d4_work.py
+++ w5_chapter5_modelling_objectives/w5d4_work.py
@@ -99,3 +99,131 @@ class CLIPVisionEmbeddings(nn.Module):
 if MAIN:
     w5d4_tests.test_vision_embeddings(CLIPVisionEmbeddings)
 #%%
+def gelu_sigmoid_approximation(x: t.Tensor) -> t.Tensor:
+    '''Return sigmoid approximation of GELU of input tensor x with same shape.'''
+    return x * t.sigmoid(1.702 * x)
+
+
+def plot_gelu_approximation(x: t.Tensor):
+    (fig, (ax0, ax1)) = plt.subplots(nrows=2, figsize=(12, 12))
+    actual = F.gelu(x)
+    approx = gelu_sigmoid_approximation(x)
+    diff = (actual - approx).abs()
+    x_cpu = x.cpu()
+    ax0.plot(x_cpu, diff.cpu(), label="absolute error")
+    ax0.legend()
+    ax1.plot(x_cpu, actual.cpu(), label="exact", alpha=0.5)
+    ax1.plot(x_cpu, approx.cpu(), label="sigmoid", alpha=0.5)
+    ax1.legend()
+    ax1.set(xlabel=f"x ({x.dtype})")
+
+
+if MAIN:
+    x = t.linspace(-5, 5, 400)
+    plot_gelu_approximation(x)
+    if t.cuda.is_available():
+        x16 = t.linspace(-5, 5, 400, dtype=t.float16, device=device)
+        plot_gelu_approximation(x16)
+
+#%%
+class CLIPMLP(nn.Module):
+    fc1: nn.Linear
+    fc2: nn.Linear
+
+    def __init__(self, config: Union[CLIPVisionConfig, CLIPTextConfig]):
+        '''Initialize parent class, then assign fully-connected layers based
+        on shape in input config'''
+        super().__init__()
+        self.fc1 = nn.Linear(config.hidden_size, config.intermediate_size)
+        self.fc2 = nn.Linear(config.intermediate_size, config.hidden_size)
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''Run forward pass of MLP, including fully-connected layers and non-linear
+        activations where appropriate'''
+        x = self.fc1(x)
+        x = gelu_sigmoid_approximation(x)
+        x = self.fc2(x)
+        return x
+
+
+if MAIN:
+    w5d4_tests.test_mlp(CLIPMLP)
+# %%
+class CLIPAttention(nn.Module):
+    num_heads: int
+    head_size: int
+    q_proj: nn.Linear
+    k_proj: nn.Linear
+    v_proj: nn.Linear
+    out_proj: nn.Linear
+    dropout: nn.Dropout
+
+    def __init__(self, config: Union[CLIPVisionConfig, CLIPTextConfig]):
+        '''Assign values from input config to class member variables as appropriate'''
+        super().__init__()
+        self.head_size = config.hidden_size / config.num_attention_heads
+        self.num_heads = config.num_attention_heads
+        assert self.head_size % self.num_heads == 0
+        self.q_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=True)
+        self.k_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=True)
+        self.v_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=True)
+        self.out_proj = nn.Linear(config.hidden_size, config.hidden_size, bias=True)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def attention_pattern_pre_softmax(self, x: t.Tensor) -> t.Tensor:
+        '''
+        Return the attention pattern after scaling but before softmax.
+
+        pattern[batch, head, q, k] should be the match between a query at sequence position q and 
+        a key at sequence position k.
+        '''
+        Q = self.q_proj(x)
+        K = self.k_proj(x)
+
+        new_Q = rearrange(
+            Q, 'batch seq (nheads headsize) -> batch nheads seq headsize', nheads=self.num_heads
+        )
+        new_K = rearrange(
+            K, 'batch seq (nheads headsize) -> batch nheads seq headsize', nheads=self.num_heads
+        )
+
+        einsum_eq = (
+            'batches nheads seq_Q head_size, '
+            'batches nheads seq_K head_size -> '
+            'batches nheads seq_Q seq_K'
+        )
+        attention_scores = einsum(einsum_eq, new_Q, new_K)
+        attention_scores /= (self.head_size ** 0.5)
+        return attention_scores
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        '''
+        Perform forward pass through attention layer, computing attention pattern and 
+        value projections to combine into output. 
+        Remember to apply dropout.'''
+        attention_scores = self.attention_pattern_pre_softmax(x)
+        attention_probabilities = nn.functional.softmax(
+            attention_scores, dim=-1
+        )
+        dropped_probabilities = self.dropout(attention_probabilities)
+        V = self.v_proj(x)
+        new_V = rearrange(
+            V, 'batch seq (nheads headsize) -> batch nheads seq headsize', nheads=self.num_heads
+        )
+        attention_values = einsum(
+            'batches nheads seq_Q seq_K, batches nheads seq_K head_size -> '
+            'batches seq_Q nheads head_size', 
+            dropped_probabilities, 
+            new_V
+        )
+        attention_rearranged = rearrange(
+            attention_values, 
+            'batches seq_Q nheads head_size -> batches seq_Q (nheads head_size)'
+        )
+        attention_times_o = self.out_proj(attention_rearranged)
+        return attention_times_o
+
+
+if MAIN:
+    w5d4_tests.test_vision_attention(CLIPAttention)
+# %%
